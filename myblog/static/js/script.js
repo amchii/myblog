6e65e4f5c371cd95792c32ddb14301cb50233561
amchii <finethankuandyou@gmail.com>
2019-09-30 16:57:15 +0800

    去掉referrer头以使用新浪图床,增加图片自适应改变大小

--- myblog/static/js/script.js
+++ myblog/static/js/script.js
@@ -2,7 +2,19 @@ $(function () {
     function render_time() {
         return moment($(this).data('timestamp')).format('lll')
     }
+
     $('[data-toggle="tooltip"]').tooltip(
         {title: render_time}
     );
+
+    var imgs = $("img");
+    var viewWidth = $(".col-sm-8").width();
+
+    function scale(img) {
+        if (img.width > viewWidth) {
+            img.width = viewWidth;
+        }
+    }
+
+    $.map(imgs, scale);
 });
